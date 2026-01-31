c9d2f122c623f61a1c939f41d721fb0b754ead85
TOMAZ-DIONISIO <tomaz.mvdionisio@gmail.com>
2026-01-31 02:21:28 -0800

    Random starting position thingie but broken?!

--- scenes/game_scene/THE_LEVEL/character_body_2d_2.gd
+++ scenes/game_scene/THE_LEVEL/character_body_2d_2.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+var new_position
+
+func _ready():
+	position = random_position()
 
 
 func _physics_process(_delta: float) -> void:
@@ -17,3 +21,10 @@ func _physics_process(_delta: float) -> void:
 	
 
 	move_and_slide()
+
+func random_position():
+	return Vector2(
+		randi_range(%PlayArea.position.x,%PlayArea.position.x + %PlayArea.shape.size.x),
+		randi_range(%PlayArea.position.y,%PlayArea.position.y + %PlayArea.shape.size.y)
+	)
+	
