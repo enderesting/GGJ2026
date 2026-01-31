97fd8cfd4d0e5c21862136026feb4b5f9f9383d4
TOMAZ-DIONISIO <tomaz.mvdionisio@gmail.com>
2026-01-31 10:33:02 -0800

    No longer bugged uwu

--- scenes/game_scene/THE_LEVEL/character_body_2d_2.gd
+++ scenes/game_scene/THE_LEVEL/character_body_2d_2.gd
@@ -3,15 +3,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var new_position
-
 func _ready():
 	position = random_position()
 
-
 func _physics_process(_delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
 	direction = direction.round().normalized()
 	if direction:
@@ -19,12 +14,11 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
-
 	move_and_slide()
 
 func random_position():
 	return Vector2(
-		randi_range(%PlayArea.position.x,%PlayArea.position.x + %PlayArea.shape.size.x),
-		randi_range(%PlayArea.position.y,%PlayArea.position.y + %PlayArea.shape.size.y)
+		randi_range(%PlayArea.position.x - %PlayArea.shape.size.x/2, %PlayArea.shape.size.x),
+		randi_range(%PlayArea.position.y - %PlayArea.shape.size.y/2, %PlayArea.shape.size.y)
 	)
 	
