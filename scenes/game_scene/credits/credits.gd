66d0b5a700d6341883562ffa106d7d67dc998dbe
diogotito <diogotitomarques@gmail.com>
2026-02-01 12:06:15 +0000

    credit pino

--- scenes/game_scene/credits/credits.gd
+++ scenes/game_scene/credits/credits.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _ready() -> void:
+	$RichTextLabel.visible_ratio = 0.0
+	create_tween().tween_property($RichTextLabel, ^"visible_ratio", 1.0, 0.25)
+
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_scene/main_menu/main_menu2.tscn")
