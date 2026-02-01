622df58b8fe4ffac6736ab09f200b352f8c7e0e0
enderesting <datendza@gmail.com>
2026-02-01 10:55:33 +0000

    player dies

--- scenes/game_scene/THE_LEVEL/main.gd
+++ scenes/game_scene/THE_LEVEL/main.gd
@@ -13,3 +13,14 @@ func kys() -> void:
 	Crossfade.fade_in()
 	Crossfade.faded_out.disconnect(kys)
 	
+
+
+func _on_player_tree_exiting() -> void:
+	Crossfade.fade_out()
+	Crossfade.faded_out.connect(player_dies)
+	
+func player_dies() -> void:
+	get_tree().change_scene_to_file("res://scenes/game_scene/endings/seer_win.tscn")
+	Crossfade.fade_in()
+	Crossfade.faded_out.disconnect(player_dies)
+	
