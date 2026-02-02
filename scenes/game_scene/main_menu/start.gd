a4d7dafc971f7f26de66c175505cb4224b523c4e
diogotito <diogotitomarques@gmail.com>
2026-02-02 19:24:52 +0000

    Avoid redundant cross_fade() in "start" button

--- scenes/game_scene/main_menu/start.gd
+++ scenes/game_scene/main_menu/start.gd
@@ -4,10 +4,13 @@ func _ready() -> void:
 	grab_focus()
 
 func _pressed() -> void:
+	# Prevent reduntant calls to Crossfade.fade_out()
+	if Crossfade.faded_out.is_connected(callLevel):
+		return
+
 	Crossfade.fade_out()
-	Crossfade.faded_out.connect(callLevel)
+	Crossfade.faded_out.connect(callLevel, ConnectFlags.CONNECT_ONE_SHOT)
 
 func callLevel():
 	get_tree().change_scene_to_file("res://scenes/game_scene/THE_LEVEL/THE_LEVEL.tscn")
 	Crossfade.fade_in()
-	Crossfade.faded_out.disconnect(callLevel)
