c1d5820c1536ec29687415f51c46ea573a8f5dd8
diogotito <diogotitomarques@gmail.com>
2026-02-02 23:52:43 +0000

    back button everywhere

--- scenes/game_scene/backbutton.gd
+++ scenes/game_scene/backbutton.gd
@@ -2,3 +2,6 @@ extends Button
 
 func _ready() -> void:
 	grab_focus()
+
+func _pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game_scene/main_menu/main_menu2.tscn")
--- scenes/game_scene/credits/credits.gd
+++ scenes/game_scene/credits/credits.gd
@@ -3,6 +3,3 @@ extends Control
 func _ready() -> void:
 	$RichTextLabel.visible_ratio = 0.0
 	create_tween().tween_property($RichTextLabel, ^"visible_ratio", 1.0, 0.25)
-
-func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://scenes/game_scene/main_menu/main_menu2.tscn")
