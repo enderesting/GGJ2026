fe450eb0c73113b7500d4fc7f51a231c42d96351
diogotito <diogotitomarques@gmail.com>
2026-02-02 19:06:00 +0000

    Disable quitting on web

--- scenes/game_scene/main_menu/quit.gd
+++ scenes/game_scene/main_menu/quit.gd
@@ -1,6 +1,14 @@
 extends Button
 
+func _ready() -> void:
+	# Disable the button on web
+	disabled = OS.has_feature("web")
 
-# Called when the node enters the scene tree for the first time.
+
+## Exits the game, unless it's running on web, as that would just leave the game
+## frozen on the page
 func _pressed() -> void:
+	if OS.has_feature("web"):
+		return
+
 	get_tree().quit()
