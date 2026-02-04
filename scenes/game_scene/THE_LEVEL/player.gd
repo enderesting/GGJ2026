bebcd6e1894afa1fcfe4ccbd10190540981397dc
diogotito <diogotitomarques@gmail.com>
2026-02-04 00:27:39 +0000

    Keep player flip_h

--- scenes/game_scene/THE_LEVEL/player.gd
+++ scenes/game_scene/THE_LEVEL/player.gd
@@ -6,15 +6,15 @@ func _exit_tree() -> void:
 	EventBus.player_killed.emit()
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
 	direction = direction.round().normalized()
 	if direction:
 		velocity = direction * SPEED
+		if velocity.x:
+			get_child(0).flip_h = velocity.x > 0
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 	
 	move_and_slide()
-	
-	super(delta)
 	
