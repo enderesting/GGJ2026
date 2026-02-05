eeb1427d227bec10eb4cff4fab6c4560bfae51eb
TOMAZ-DIONISIO <tomaz.mvdionisio@gmail.com>
2026-02-05 14:25:59 +0000

    beep_boop implementation and halfway done on throw

--- scenes/game_scene/THE_LEVEL/player.gd
+++ scenes/game_scene/THE_LEVEL/player.gd
@@ -5,6 +5,15 @@ class_name Player
 func _exit_tree() -> void:
 	EventBus.player_killed.emit()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("beep_boop"):
+		$AnimatedSprite2D.play("beep_boop")
+		$beep_boop.play()
+	if event.is_action_pressed("throw"):
+		#throwing animation goes here
+		$throw.play()
+	$AnimatedSprite2D.play("RESET")
+
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
