b80670390df73393a88177a13832a5773f5084e8
diogotito <diogotitomarques@gmail.com>
2026-02-05 19:37:04 +0000

    Add drop_screw.png import metadata

--- scenes/robots/player/projectiles/projectile.gd
+++ scenes/robots/player/projectiles/projectile.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 # spawns and moves in an arc to the overseer coordinates, then generates a lil explosion
-@export var overseer_location = PackedScene
+var overseer_location : Vector2
 
 func _ready() -> void:
 	var part = randi_range(0,2)
 	var parts = $AnimatedSprite2D.get_sprite_frames()
 	$AnimatedSprite2D.play(parts.get_animation_names().get(part))
 	move_to_overseer()
+
+func _reached_overseer():
 #	explosion animation goes here
 	queue_free()
 
 func move_to_overseer():
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "position", overseer_location.position, 1.0)
+	tween.tween_property(self, "position", overseer_location, 1.0)
+	tween.finished.connect(_reached_overseer)
