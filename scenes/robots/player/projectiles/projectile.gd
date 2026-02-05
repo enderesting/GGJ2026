0e18d40a3604066e3167d165f934dba2ee10559b
diogotito <diogotitomarques@gmail.com>
2026-02-05 20:16:05 +0000

    Major scale!

--- scenes/robots/player/projectiles/projectile.gd
+++ scenes/robots/player/projectiles/projectile.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 	var part = randi_range(0,2)
 	var parts = $AnimatedSprite2D.get_sprite_frames()
 	$AnimatedSprite2D.play(parts.get_animation_names().get(part))
+	%sfx.pitch_scale = 0.75 * [1.00, 1.12, 1.26, 1.33, 1.50, 1.68, 1.89, 2.00].pick_random()
 	move_to_overseer()
 
 func _process(delta: float) -> void:
@@ -15,6 +16,7 @@ func _process(delta: float) -> void:
 func _reached_overseer():
 #	explosion animation goes here
 	$AnimatedSprite2D.visible = false
+	
 	%sfx.play()
 	%sfx.finished.connect(queue_free)
 
