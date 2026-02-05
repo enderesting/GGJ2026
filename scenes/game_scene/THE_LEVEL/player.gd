ba269be8a06fc223b4d09bc3d6115f0286ab82fa
TOMAZ-DIONISIO <tomaz.mvdionisio@gmail.com>
2026-02-05 14:54:00 +0000

    fix pop in beep boop fix beep boop fix throw fix f

--- scenes/game_scene/THE_LEVEL/player.gd
+++ scenes/game_scene/THE_LEVEL/player.gd
@@ -6,13 +6,14 @@ func _exit_tree() -> void:
 	EventBus.player_killed.emit()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("beep_boop"):
+	if event.is_action_pressed("beep_boop") and $AnimatedSprite2D.animation == "RESET":
 		$AnimatedSprite2D.play("beep_boop")
 		$beep_boop.play()
+		await $AnimatedSprite2D.animation_finished
+		$AnimatedSprite2D.play("RESET")
 	if event.is_action_pressed("throw"):
 		#throwing animation goes here
 		$throw.play()
-	$AnimatedSprite2D.play("RESET")
 
 
 func _physics_process(_delta: float) -> void:
