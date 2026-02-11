a2fb5f6b08ddd2398a012f2d75150d2b3102d361
diogotito <diogotitomarques@gmail.com>
2026-02-11 01:02:01 +0000

    Split countdown label into a label per char

--- scenes/game_scene/THE_LEVEL/countdown.gd
+++ scenes/game_scene/THE_LEVEL/countdown.gd
@@ -3,11 +3,11 @@ extends Node2D
 const RUNNING_OUT_OF_TIME_AT: int = 20 # seconds
 const BLINK_FREQ: int = 2
 
-@onready var label: Label = $Label
+@onready var labels := self.find_children("*", "Label")
 @onready var timer: Timer = $Timer
 
 @onready var timer_fg_colors: Array[Color] = [
-	label.get_theme_color(&"font_color"),  # Default color
+	labels[0].get_theme_color(&"font_color"),  # Default color
 	Color.RED,  # The alternating color when running out of time
 ]
 
@@ -25,9 +25,12 @@ func time_left_to_live():
 
 func _process(_delta: float) -> void:
 	# Update text, formatted as MINS:SECS
-	label.text = "%02d:%02d" % time_left_to_live()
+	var text := "%02d:%02d" % time_left_to_live()
+	for i in len(labels):
+		labels[i].text = text[i]
 	
 	# Update foreground color, making it blink when running out of time
 	if timer.time_left <= RUNNING_OUT_OF_TIME_AT:
 		var color_i := int(timer.time_left * BLINK_FREQ) % len(timer_fg_colors)
-		label.add_theme_color_override(&"font_color", timer_fg_colors[color_i])
+		for label in labels:
+			label.add_theme_color_override(&"font_color", timer_fg_colors[color_i])
